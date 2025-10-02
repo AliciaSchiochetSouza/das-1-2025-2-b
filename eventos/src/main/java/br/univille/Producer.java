b8a9387cf785d79543171f0981701cd95bc5c535
aliciaschiochetsouza <136520076+AliciaSchiochetSouza@users.noreply.github.com>
2025-10-02 22:57:51 +0000

    Add producer e consumer

--- eventos/src/main/java/br/univille/Producer.java
+++ eventos/src/main/java/br/univille/Producer.java
@@ -1,36 +1,36 @@
 package br.univille;
 
 import com.azure.core.amqp.AmqpTransportType;
-import com.azure.identity.DefaultAzureCredential;
-import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.messaging.servicebus.ServiceBusClientBuilder;
 import com.azure.messaging.servicebus.ServiceBusMessage;
 import com.azure.messaging.servicebus.ServiceBusSenderClient;
 
-
-public class Publisher {
+public class Producer {
     public static void main(String[] args) {
-        var topicName = "topic-das1-b";
-        var servidor = "sbdas12025a.servicebus.windows.net";
-        
-        DefaultAzureCredential credential = 
-            new DefaultAzureCredentialBuilder().build();
+        var servidor="sb-das1-2025.servicebus.windows.net";
+        var chave= System.getenv("CHAVE");
+        var queue = "queue-aliciaschiochet";
 
-        String chave = System.getenv("CHAVE");
-       ServiceBusSenderClient senderClient =
+
+         ServiceBusSenderClient senderClient =
             new ServiceBusClientBuilder()
             .fullyQualifiedNamespace(servidor)
             .transportType(AmqpTransportType.AMQP_WEB_SOCKETS)
             // .credential(credential)
             .connectionString(chave)
             .sender()
-            .topicName(topicName)
+            .queueName(queue)
             .buildClient();
             
         senderClient.sendMessage(
             new ServiceBusMessage(
-                "Alícia: Uauuuu!"));
-        
-        
+                "1: Alícia: Uauuuu!"));
+                senderClient.sendMessage(
+            new ServiceBusMessage(
+                "2: Alícia: Uauuuu!"));
+                senderClient.sendMessage(
+            new ServiceBusMessage(
+                "3: Alícia: Uauuuu!"));
     }
+
 }
